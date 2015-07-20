71b35a78a5a7fe901d755a5a77ba6c670f2dbf7e
DavidKohav <david@gizra.com>
2015-07-20 11:38:19 +0300

    Update the visual monitor.

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -5,13 +5,12 @@ var shoovWebdrivercss = require('shoov-webdrivercss');
 // This can be executed by passing the environment argument like this:
 // PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
 // PROVIDER_PREFIX=browserstack SELECTED_CAPS=chrome mocha
+
 var capsConfig = {
   'chrome': {
-    'browser' : 'Chrome',
-    'browser_version' : '42.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1024x768'
+    'browserName' : 'chrome',
+    'version' : '43.0',
+    'platform' : 'OS X 10.10'
   },
   'ie11': {
     'browser' : 'IE',
@@ -20,7 +19,7 @@ var capsConfig = {
     'os_version' : '7',
     'resolution' : '1024x768'
   }
-}
+};
 
 var selectedCaps = process.env.SELECTED_CAPS || undefined;
 var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
@@ -45,9 +44,9 @@ describe('Visual monitor testing', function() {
 
   it('should show the buy now page',function(done) {
     client
-      .url(baseUrl + "/platform/buy-now")
-      // Hide the cookies popup.
+      .url(baseUrl + '/platform/buy-now')
       .click('#popup-buttons button')
+      .pause(5000)
       .webdrivercss(testName + '.buy-now', {
         name: '1',
         exclude: [
@@ -55,7 +54,7 @@ describe('Visual monitor testing', function() {
           '.cost',
           '#number'
         ],
-        screenWidth: selectedCaps == 'chrome' ? [320, 640, 960, 1200] : undefined,
+        screenWidth: selectedCaps == 'chrome' ? [320,640,960, 1200] : undefined
       }, shoovWebdrivercss.processResults)
       .call(done);
   });
