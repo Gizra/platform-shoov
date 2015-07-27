17db162d7b622f8fce8ccfdcf2407680a3ffd117
Amitaibu <amitai@gizra.com>
2015-07-27 14:18:32 +0300

    Update yo shoov

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -3,14 +3,17 @@
 var shoovWebdrivercss = require('shoov-webdrivercss');
 
 // This can be executed by passing the environment argument like this:
-// PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
 // PROVIDER_PREFIX=browserstack SELECTED_CAPS=chrome mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=iphone5 mocha
 
 var capsConfig = {
   'chrome': {
-    'browserName' : 'chrome',
-    'version' : '43.0',
-    'platform' : 'OS X 10.10'
+    'browser' : 'Chrome',
+    'browser_version' : '42.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'resolution' : '1024x768'
   },
   'ie11': {
     'browser' : 'IE',
@@ -18,6 +21,17 @@ var capsConfig = {
     'os' : 'Windows',
     'os_version' : '7',
     'resolution' : '1024x768'
+  },
+  'iphone5': {
+    'browser' : 'Chrome',
+    'browser_version' : '42.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'chromeOptions': {
+      'mobileEmulation': {
+        'deviceName': 'Apple iPhone 5'
+      }
+    }
   }
 };
 
